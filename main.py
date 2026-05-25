a020733c4914bfd4c1de671451526cf7fb063023
Syahraz Fiqar Alamsyah <syahrazfa@gmail.com>
2026-05-25 22:30:13 +0700

    Ini main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from tools import  screener
+import backtest
+from tools import screener
+import matplotlib.pyplot as plt
 
-print("Trading Log")
+
+
+def plotting(df):
+plt.figure(figsize=(20,10))
+plt.plot(df['CLose'], label='Close Price', alpha=0.5)
+plt.plot(df['fast_sma'], label='SMA', linestyle='dashed', linewidth=2)
+plt.plot(df['medium_sma'], label='SMA', linestyle='dashed', linewidth=2)
+plt.plot(df['slow_sma'], label='SMA', linestyle='dashed', linewidth=2)
